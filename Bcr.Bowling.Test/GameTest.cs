2f7ccc46f10bbf9aa7c8d1975346a8726544f48f
Blake Ramsdell <blake.ramsdell@onelogin.com>
2018-11-02 21:09:24 -0700

    Added gutterball champ.

--- Bcr.Bowling.Test/GameTest.cs
+++ Bcr.Bowling.Test/GameTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System.Linq;
+
 namespace Bcr.Bowling.Test
 {
     [TestClass]
@@ -10,6 +12,18 @@ namespace Bcr.Bowling.Test
         {
             var game = new Game();
 
+            Assert.AreEqual(0, game.Score);
+        }
+
+        [TestMethod]
+        public void TestGutterballs()
+        {
+            var game = new Game();
+
+            // Man, this fine player needs those bumpers in the gutters
+            // 20 throws of zero (two for each frame, ten frames)
+
+            game.Throw(Enumerable.Repeat(0, 20));
             Assert.AreEqual(0, game.Score);
         }
     }
--- Bcr.Bowling/Game.cs
+++ Bcr.Bowling/Game.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 namespace Bcr.Bowling
 {
     public class Game
     {
         public int Score { get; set; }
+
+        public void Throw(IEnumerable<int> throws)
+        {
+        }
     }
 }
