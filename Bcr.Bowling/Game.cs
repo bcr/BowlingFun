58d3383d03b06b0fe1af72d35104827940bdbe5b
Blake Ramsdell <blake.ramsdell@onelogin.com>
2018-11-03 09:23:39 -0700

    A much simplified version that just does the final score.

--- Bcr.Bowling/Game.cs
+++ Bcr.Bowling/Game.cs
@@ -6,31 +6,50 @@ namespace Bcr.Bowling
 {
     public class Game
     {
-        public int Score { get { return frames.Sum(frame => frame.Score); } }
+        public int Score
+        {
+            get
+            {
+                int finalScore = 0;
+                int index = 0;
+                int frameNumber = 1;
 
-        private List<Frame> frames = new List<Frame> { new Frame() };
+                while ((index < throwIndex) && (frameNumber <= 10))
+                {
+                    int nextTwoBallsTotal = (finalThrows[index] + finalThrows[index + 1]);
+
+                    finalScore += nextTwoBallsTotal;
+
+                    if (finalThrows[index] == 10)
+                    {
+                        // Steerike
+                        finalScore += finalThrows[index + 2];
+                        index += 1;
+                    }
+                    else
+                    {
+                        finalScore += (nextTwoBallsTotal == 10) ? finalThrows[index + 2] : 0;
+                        index += 2;
+                    }
+
+                    ++frameNumber;
+                }
+                return finalScore;
+            }
+        }
+
+        // Good ole magic 21 throws. If you throw all zeroes for the first
+        // nine frames, that's 18 throws. In the tenth if you suddenly get a
+        // strike, that's 19 throws, plus two more throws to close the tenth
+        // frame, a total of 21.
+        private int[] finalThrows = new int[21];
+        private int throwIndex = 0;
 
         public void Throw(IEnumerable<int> throws)
         {
             foreach (var _throw in throws)
             {
-                Frame lastFrame = null;
-                int frameCount = 0;
-
-                foreach (var frame in frames)
-                {
-                    if (frame.WantsMoreThrows)
-                    {
-                        frame.Throw(_throw);
-                    }
-                    lastFrame = frame;
-                    ++frameCount;
-                }
-
-                if ((lastFrame.IsPrimaryThrowsDone) && (frameCount < 10))
-                {
-                    frames.Add(new Frame());
-                }
+                finalThrows[throwIndex++] = _throw;
             }
         }
     }
